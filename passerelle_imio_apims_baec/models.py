c177d51f88cc1b127086ce0974b6e209cc485bbf
Nicolas Selva <nicolas.selva@imio.be>
2022-10-18 15:52:26 +0200

    [TELE-1507]Refactor apims BAEC for production

--- passerelle_imio_apims_baec/models.py
+++ passerelle_imio_apims_baec/models.py
@@ -6,12 +6,13 @@ from django.core.exceptions import ValidationError
 from passerelle.base.models import BaseResource
 from passerelle.utils.api import endpoint
 from passerelle.utils.jsonresponse import APIError
+from requests import RequestException
 
 
 def validate_url(value):
     if value.endswith("/"):
         raise ValidationError(
-            ('%(value)s ne dois pas finir avec un "/"'),
+            '%(value)s ne dois pas finir avec un "/"',
             params={'value': value},
         )
 
@@ -23,11 +24,13 @@ class ApimsBaecConnector(BaseResource):
     Attributes
     ----------
     url : str
-        a formatted string to print out what the animal says
+        url used to connect to APIMS BAEC
     username : str
-        username used to connect to apims
+        username used to connect to APIMS BAEC
     password : str
-        password used to connect to apims
+        password used to connect to APIMS BAEC
+    municipality_token : str
+        token used to identify municipality to APIMS BAEC
 
     Methods
     -------
@@ -47,12 +50,20 @@ class ApimsBaecConnector(BaseResource):
     username = models.CharField(
         max_length=128,
         blank=True,
+        help_text="Utilisateur APIMS BAEC",
         verbose_name="Utilisateur",
     )
     password = models.CharField(
         max_length=128,
         blank=True,
+        help_text="Mot de passe APIMS BAEC",
         verbose_name="Mot de passe",
+    )
+    municipality_token = models.CharField(
+        max_length=128,
+        blank=True,
+        help_text="Token d'identification de l'organisme dans APIMS BAEC",
+        verbose_name="Token d'identification",
     )
     api_description = "Connecteur permettant d'intéragir avec APIMS BAEC"
     category = "Connecteurs iMio"
@@ -64,23 +75,31 @@ class ApimsBaecConnector(BaseResource):
     def session(self):
         session = requests.Session()
         session.auth = (self.username, self.password)
-        session.headers.update({"Accept": "application/json"})
+        session.headers.update({
+            "Accept": "application/json",
+            "X-IMIO-MUNICIPALITY-TOKEN": self.municipality_token
+        })
         return session
 
     @endpoint(
         name="test",
         perm="can_access",
-        description="Valider la connexion entre APIMS et Publik",
+        description="Test Connexion",
+        long_description="Valider la connexion entre APIMS et Publik",
         serializer_type="json-api",
+        display_order=0,
+        display_category="Test"
     )
     def test(self, request):
-        url = self.url
+        url = f"{self.url}/test"
+        self.logger.info("Lancement d'un test")
         return self.session.get(url).json()
 
     @endpoint(
         name="list-person-documents",
         perm="can_access",
-        description="Liste les documents disponibles pour une personne identifiée par son numéro de registre national",
+        description="Lister les documents",
+        long_description="Liste les documents disponibles pour une personne identifiée par son numéro de registre national",
         parameters={
             "rn": {
                 "description": "Numéro de Registre national",
@@ -88,6 +107,8 @@ class ApimsBaecConnector(BaseResource):
             },
         },
         serializer_type="json-api",
+        display_order=0,
+        display_category="Documents",
     )
     def list_person_documents(self, request, rn):
         """ Gets available documents for the person
@@ -100,9 +121,28 @@ class ApimsBaecConnector(BaseResource):
         dict
             all document with reference and available type
         """
-        url = f"{self.url}/{rn}"
-        return self.session.get(url).json()
+        url = f"{self.url}/civil-status-documents/{rn}"
 
+        self.logger.info("Liste des documents disponibles")
+        try:
+            response = self.session.get(url)
+        except RequestException as e:
+            self.logger.warning(f'BAEC APIMS Error: {e}')
+            raise APIError(f'BAEC APIMS Error: {e}')
+
+        json_response = None
+        try:
+            json_response = response.json()
+        except ValueError:
+            self.logger.warning('BAEC APIMS Error: bad JSON response')
+            raise APIError('BAEC APIMS Error: bad JSON response')
+
+        try:
+            response.raise_for_status()
+        except RequestException as e:
+            self.logger.warning(f'BAEC APIMS Error: {e} {json_response}')
+            raise APIError(f'BAEC APIMS Error: {e} {json_response}')
+        return json_response
 
     @endpoint(
         name="read-document",
@@ -123,6 +163,8 @@ class ApimsBaecConnector(BaseResource):
                 "example_value": "E",
             },
         },
+        display_order=1,
+        display_category="Documents"
     )
     def read_document(self, request, rn, certificate_reference, certificate_type):
         """ Get asked document as PDF
@@ -138,8 +180,28 @@ class ApimsBaecConnector(BaseResource):
         -------
         PDF document
         """
-        url = f"{self.url}/{rn}/{certificate_reference}/{certificate_type}"
-        response = requests.get(url, auth=(self.username, self.password))
-        if response.status_code >= 400:
-           raise APIError("502 Bad gateway", http_status=502, data={"target_url": url, "error": response.status_code})
-        return HttpResponse(response.content, content_type="application/pdf")
+
+        url = f"{self.url}/civil-status-documents/{rn}/{certificate_reference}/{certificate_type}"
+
+        self.logger.info("Récupération du document PDF")
+        try:
+            response = requests.get(url, auth=(self.username, self.password), headers={
+                "X-IMIO-MUNICIPALITY-TOKEN": self.municipality_token
+            })
+        except Exception as e:
+            self.logger.warning(f'BAEC APIMS Error: {e}')
+            raise APIError(f'BAEC APIMS Error: {e}')
+
+        pdf_response = None
+        try:
+            pdf_response = HttpResponse(response.content, content_type="application/pdf")
+        except ValueError:
+            self.logger.warning('BAEC APIMS Error: bad PDF response')
+            raise APIError('BAEC APIMS Error: bad PDF response')
+
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            self.logger.warning(f'BAEC APIMS Error: {e}')
+            raise APIError(f'BAEC APIMS Error: {e}')
+        return pdf_response
